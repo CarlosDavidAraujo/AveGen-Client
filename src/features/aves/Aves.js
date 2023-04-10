91e3464fe1e4cc797d89ead5b6d2daa17b4c2be3
CarlosDavidAraujo <cdavidav19@gmail.com>
2023-04-10 17:29:51 -0300

    conectando a api

---
+++ src/features/aves/Aves.js
@@ -0,0 +1,7 @@
+export default function Aves() {
+  return (
+    <>
+      <a>ave</a>
+    </>
+  );
+}
--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import api from "../../api";
+
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    async function getData(){
+      const data = await api.get('/ave');
+      console.log(data);
+      setData(data);
+    }
+    getData();
+  },[]);
+  
   return (
   <>
-    <a href="">lista de aves</a>
+    {data.map((ave) => <a>`Ave: ${ave.id}`</a>)}
   </>
   );
 }
